5d41fb26cc381cd4f272d4bd9bc1a83fd7f571b2
IntelligenzaArtificiale <ciciarelli.1897655@studenti.uniroma1.it>
2024-01-05 22:19:49 +0100

    Refactor chat database functions

--- dbmemory.py
+++ dbmemory.py
@@ -17,34 +17,39 @@ def convert_messages_to_json(messages):
 
 # Funzione per convertire i messaggi da JSON a lista di messaggi
 def convert_json_to_messages(json_data):
-    return json.loads(json_data)
+    if isinstance(json_data, list):
+        return json_data
+    elif isinstance(json_data, str):
+        return json.loads(json_data)
+    else:
+        return []
 
 # Inserimento dei messaggi nel database
-def insert_chat(client: Client, id_chat, messages, autore):
-    messages_json = convert_messages_to_json(messages)
-    return client.table("Database per CHAT CLIENTI").insert(
-        [{"id_chat": id_chat, "conversazione": messages_json, "autore": autore}]
+def insert_chat(client: Client, id_chat, autore):
+    return client.table("CHAT").insert(
+        [{"id_chat": id_chat, "autore": autore}]
     ).execute()
 
 # Recupero dei messaggi dal database
 def get_chat(client: Client, autore, id_chat):
-    result = client.table("Database per CHAT CLIENTI").select("*").eq("autore", autore).eq("id_chat", id_chat).execute()
-    if len(result["data"]) > 0:
-        chat_data = result["data"][0]
+    result = client.table("CHAT").select("*").eq("autore", autore).eq("id_chat", id_chat).execute()
+    print(result)
+    if len(result.data) > 0:
+        chat_data = result.data[0]
         chat_messages_json = chat_data["conversazione"]
         messages = convert_json_to_messages(chat_messages_json)
         return messages
-    return None
+    return []
 
 def update_chat(client: Client, id_chat, conversazione):
-    return client.table("Database per CHAT CLIENTI").update(
+    return client.table("CHAT").update(
         {"conversazione": conversazione}
     ).eq("id_chat", id_chat).execute()
 
 def delete_chat(client: Client, id_chat):
-    return client.table("Database per CHAT CLIENTI").delete().eq("id_chat", id_chat).execute()
+    return client.table("CHAT").delete().eq("id_chat", id_chat).execute()
 
 def get_all_chat(client: Client, autore):
-    return client.table("Database per CHAT CLIENTI").select("*").eq("autore", autore).execute()
+    return client.table("CHAT").select("*").eq("autore", autore).execute()
 
 
--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import openai
 import streamlit as st
 import random
 from dbmemory import init_connection, insert_chat, get_chat, update_chat, delete_chat, get_all_chat
-
+import asyncio
 
 st.title('🤖💬 OpenAI Chatbot')
 
@@ -14,22 +14,20 @@ if "OPENAI_API_KEY" not in st.session_state:
     start = st.container()
     with start:
         nome_utente = st.text_input('Inserisci il tuo nome utente : ')
-        modello = st.selectbox('Seleziona il modello di IA', ('Google Gemini PRO', 'OpenAI GPT3.5'))
         api_key = st.text_input('Inserisci OpenAI API KEY:', type='password')
         
-        if len(api_key)>3 and len(nome_utente)>3 :
             
-            if st.button("login"):
-                if not (api_key.startswith('sk-') and len(api_key)==51):
-                    st.warning('HEY non è corretta!', icon='⚠️')
-                else:
-                    openai.api_key = api_key
-                    st.session_state.OPENAI_API_KEY = api_key
-                    st.session_state.nome_utente = nome_utente
-                    st.session_state.supabase = init_connection()
-                    st.success('Ok perfetto entriamo!', icon='👉')
-                    time.sleep(2)
-                    st.rerun()
+        if st.button("login"):
+            if not (api_key.startswith('sk-') and len(api_key)==51):
+                st.warning('HEY non è corretta!', icon='⚠️')
+            else:
+                openai.api_key = api_key
+                st.session_state.OPENAI_API_KEY = api_key
+                st.session_state.nome_utente = nome_utente
+                st.session_state.supabase = init_connection()
+                st.success('Ok perfetto entriamo!', icon='👉')
+                time.sleep(2)
+                st.rerun()
         else:
             st.stop()
 else:
@@ -41,47 +39,75 @@ with st.sidebar:
     st.write("")
     
     # Recuperiamo tutte le chat dell'utente attuale
-    user_chats = get_all_chat(st.session_state.supabase, st.session_state.noe_utente)
-    chat_list = ["Nuova Chat"] + [chat["id_chat"] for chat in user_chats["data"]]
+    st.session_state.user_chats = get_all_chat(st.session_state.supabase, st.session_state.nome_utente)
 
-    selected_chat = st.sidebar.selectbox("Seleziona una chat:", chat_list)
+    if len(st.session_state.user_chats.data) == 0:
+        chat_list = ["Nuova Chat"]
+    else:
+        chat_list = ["Nuova Chat"] + [chat["id_chat"] for chat in st.session_state.user_chats.data]
+
+    selected_chat = st.sidebar.selectbox("Seleziona una chat:", chat_list, index=0 if "chat_id" not in st.session_state else chat_list.index(st.session_state.chat_id))
 
     if selected_chat == "Nuova Chat":
         # Logica per una nuova chat
-        new_chat_name = st.text_input("Inserisci il nome per la nuova chat:", value=f"Chat_{random.randint(1000, 9999)}")
-        if st.button("Inizia nuova chat"):
+        new_chat_name = st.text_input("Inserisci il nome per la nuova chat:")
+        if st.button("Inizia nuova chat") and len(new_chat_name) > 3:
             st.session_state.chat_id = new_chat_name
             st.session_state.messages = []
             with st.spinner("Creazione nuova chat..."):
-                insert_chat(st.session_state.supabase, new_chat_name, [], st.session_state.nome_utente)
-            st.success("Chat creata con successo!")
+                insert_chat(st.session_state.supabase, new_chat_name, st.session_state.nome_utente)
+                st.success("Chat ' {} ' creata con successo!".format(new_chat_name))
+                time.sleep(1)
+            st.rerun()
 
-    elif selected_chat in [chat ["id_chat"] for chat in user_chats["data"]]:
+    elif selected_chat in [chat ["id_chat"] for chat in st.session_state.user_chats.data]:
+        if "chat_id" not in st.session_state:
+            st.session_state.chat_id = ""
         if selected_chat != st.session_state.chat_id:
             with st.spinner("Caricamento chat..."):
                 st.session_state.chat_id = selected_chat
                 st.session_state.messages = get_chat(st.session_state.supabase, st.session_state.nome_utente, selected_chat)
-            st.success("Chat caricata con successo!")
+                st.success("Chat ' {} ' caricata con successo!".format(selected_chat))
 
+    if "user_chats" in st.session_state:
+        if st.button("Aggiorna lista chat"):
+            with st.spinner("Aggiornamento chat..."):
+                st.session_state.user_chats = get_all_chat(st.session_state.supabase, st.session_state.nome_utente)
+                st.success("Lista chat aggiornata con successo!")
+                time.sleep(1)
+            st.rerun()
 
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+        if st.button("Elimina chat"):
+            with st.spinner("Eliminazione chat..."):
+                delete_chat(st.session_state.supabase, st.session_state.chat_id)
+                st.session_state.user_chats = get_all_chat(st.session_state.supabase, st.session_state.nome_utente)
+                del st.session_state.messages
+                del st.session_state.chat_id
+                st.success("Chat eliminata con successo!")
+                st.info("Seleziona una chat dalla lista per continuare")
+                time.sleep(1)
+            st.rerun()
 
-if prompt := st.chat_input("Ciao, come posso aiutarti?"):
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    with st.chat_message("user"):
-        st.markdown(prompt)
-    with st.chat_message("assistant"):
-        message_placeholder = st.empty()
-        full_response = ""
-        for response in openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=[{"role": m["role"], "content": m["content"]}
-                      for m in st.session_state.messages], stream=True):
-            full_response += response.choices[0].delta.get("content", "")
-            message_placeholder.markdown(full_response + "▌")
-        message_placeholder.markdown(full_response)
-    st.session_state.messages.append({"role": "assistant", "content": full_response})
-    update_chat(st.session_state.supabase, st.session_state.chat_id, st.session_state.messages)
-    
+if "messages" in st.session_state:
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+if "messages" in st.session_state:
+    if prompt := st.chat_input("Ciao, come posso aiutarti?"):
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user"):
+            st.markdown(prompt)
+        with st.chat_message("assistant"):
+            message_placeholder = st.empty()
+            full_response = ""
+            for response in openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=[{"role": m["role"], "content": m["content"]}
+                        for m in st.session_state.messages], stream=True):
+                full_response += response.choices[0].delta.get("content", "")
+                message_placeholder.markdown(full_response + "▌")
+            message_placeholder.markdown(full_response)
+        st.session_state.messages.append({"role": "assistant", "content": full_response})
+        update_chat(st.session_state.supabase, st.session_state.chat_id, st.session_state.messages)
+
